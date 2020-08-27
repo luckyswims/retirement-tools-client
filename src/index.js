7c627834ed9dfdc19c18a1b8d792a4cee09edbcd
John Lucker <john.lucker@comcast.net>
2020-08-27 11:38:52 -0400

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './components/App/App'
+
+const appJsx = (
+  <App />
+)
+
+ReactDOM.render(appJsx, document.getElementById('root'))
