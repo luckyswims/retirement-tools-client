b3f80f0c1d75e8947820b2a25df72f96328772bb
John Lucker <john.lucker@comcast.net>
2020-09-10 15:41:09 -0400

    Updated form for rate options
    Updated the Present Value Calculator form to allow the user to choose
    between single rate calculation PPA three segment rates

--- src/components/Annuities/Annuities.js
+++ src/components/Annuities/Annuities.js
@@ -8,7 +8,10 @@ import { pv } from '../../api/annuities'
 const Annuities = () => {
   const [amount, setAmount] = useState()
   const [duration, setDuration] = useState()
+  const [rateType, setRateType] = useState()
   const [rate, setRate] = useState()
+  const [rate2, setRate2] = useState()
+  const [rate3, setRate3] = useState()
   const [result, setResult] = useState()
   const handleChange = event => {
     switch (event.target.name) {
@@ -21,10 +24,25 @@ const Annuities = () => {
       case "rate":
         setRate(Number(event.target.value))
         break
+      case "rate2":
+        setRate2(Number(event.target.value))
+        break
+      case "rate3":
+        setRate3(Number(event.target.value))
+        break
     }
+  }
+  const onClick = event => {
+    setRateType(event.target.value)
   }
   const onSubmit = event => {
     event.preventDefault()
+    let rates
+    if (rateType === 'single') {
+      rates = rate
+    } else if (rateType === 'PPA') {
+      rates = [rate, rate2, rate3]
+    }
     const data = {
       annuity: {
         amount,
@@ -34,6 +52,56 @@ const Annuities = () => {
     }
     pv(data)
       .then(res => setResult(res.data))
+  }
+  let ratesForm
+  if (rateType === 'single') {
+    ratesForm = (
+      <Form.Group controlId="rate">
+        <Form.Label>Interest Rate</Form.Label>
+        <Form.Control 
+          required
+          type="number"
+          step="0.01"
+          name="rate"
+          onChange={handleChange}
+        />
+      </Form.Group>
+    )
+  } else if (rateType === 'PPA') {
+    ratesForm = (
+      <Fragment>
+        <Form.Group controlId="rate">
+          <Form.Label>First Rate</Form.Label>
+          <Form.Control 
+            required
+            type="number"
+            step="0.01"
+            name="rate"
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <Form.Group controlId="rate2">
+          <Form.Label>Second Rate</Form.Label>
+          <Form.Control 
+            required
+            type="number"
+            step="0.01"
+            name="rate2"
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <Form.Group controlId="rate3">
+          <Form.Label>Third Rate</Form.Label>
+          <Form.Control 
+            required
+            type="number"
+            step="0.01"
+            name="rate3"
+            onChange={handleChange}
+          />
+        </Form.Group>
+      </Fragment>
+    )
   }
   let resultMessage
   if (result !== undefined) {
@@ -62,16 +130,25 @@ const Annuities = () => {
             onChange={handleChange}
           />
         </Form.Group>
-        <Form.Group controlId="rate">
-          <Form.Label>Interest Rate</Form.Label>
-          <Form.Control 
-            required
-            type="number"
-            step="0.01"
-            name="rate"
-            onChange={handleChange}
+        <Form.Group controlId="single">
+          <Form.Label>Single Rate</Form.Label>
+          <Form.Control
+            type="radio"
+            name="rateType"
+            value="single"
+            onClick={onClick}
           />
         </Form.Group>
+        <Form.Group controlId="PPA">
+          <Form.Label>PPA Rates</Form.Label>
+          <Form.Control
+            type="radio"
+            name="rateType"
+            value="PPA"
+            onClick={onClick}
+          />
+        </Form.Group>
+        {ratesForm}
         <Button
           variant="primary"
           type="submit"
